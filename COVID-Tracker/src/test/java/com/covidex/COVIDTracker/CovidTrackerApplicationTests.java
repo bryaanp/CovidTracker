9825178366d17ce74273e28a6061e9ba3dad356e
Bryan <48010993+bryaanp@users.noreply.github.com>
2022-08-25 21:45:48 -0700

    Complete covid-tracker
    Old update to files and commit, forgotten changes

---
+++ COVID-Tracker/src/test/java/com/covidex/COVIDTracker/CovidTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covidex.COVIDTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
